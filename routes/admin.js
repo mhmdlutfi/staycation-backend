3fabc628fe528aca8f4683e97b90fda2111a2b02
heri setiawan <hsetiawan8.hs@gmail.com>
2022-02-15 00:33:54 +0700

    add modal update

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,14 @@
-const router = require('express').Router();
-const adminController = require('../controllers/adminController');
+const router = require("express").Router();
+const adminController = require("../controllers/adminController");
 
-router.get('/dashboard', adminController.viewDashboard);
-router.get('/category', adminController.viewCategory);
+router.get("/dashboard", adminController.viewDashboard);
+// endpoint category
+router.get("/category", adminController.viewCategory);
+router.post("/category", adminController.addCategory);
+router.put("/category", adminController.editCategory);
+router.delete("/category/:id", adminController.deleteCategory);
+router.get("/bank", adminController.viewBank);
+router.get("/item", adminController.viewItem);
+router.get("/booking", adminController.viewBooking);
 
 module.exports = router;
