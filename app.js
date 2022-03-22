236477ee8d9d3dc48e66bc236a6730d14200bce8
heri setiawan <hsetiawan8.hs@gmail.com>
2022-03-22 09:15:08 +0700

    add api landing-page

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var usersRouter = require("./routes/users");
 
 // router admin
 const adminRouter = require("./routes/admin");
+const apiRouter = require("./routes/api");
 
 var app = express();
 
@@ -50,6 +51,7 @@ app.use("/users", usersRouter);
 
 // admin
 app.use("/admin", adminRouter);
+app.use("/api/v1/member", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
