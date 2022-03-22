f6b1dca51bb6f55fbd516d57e2d8b0b83f190653
Heri Setiawan <hsetiawan8.hs@gmail.com>
2022-03-23 00:27:36 +0700

    add api booking

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,30 +1,32 @@
-const Item = require('../models/Item');
-const Treasure = require('../models/Activity');
-const Traveler = require('../models/Booking');
-const Category = require('../models/Category');
-const Bank = require('../models/Bank');
+const Item = require("../models/Item");
+const Treasure = require("../models/Activity");
+const Traveler = require("../models/Booking");
+const Category = require("../models/Category");
+const Booking = require("../models/Booking");
+const Bank = require("../models/Bank");
+const Member = require("../models/Member");
 
 module.exports = {
   landingPage: async (req, res) => {
     try {
       const mostPicked = await Item.find()
-        .select('_id title country city price unit imageId')
+        .select("_id title country city price unit imageId")
         .limit(5)
-        .populate({ path: 'imageId', select: '_id imageUrl' });
+        .populate({ path: "imageId", select: "_id imageUrl" });
 
       const category = await Category.find()
-        .select('_id name')
+        .select("_id name")
         .limit(3)
         .populate({
-          path: 'itemId',
-          select: '_id title country city isPopular unit imageId',
+          path: "itemId",
+          select: "_id title country city isPopular unit imageId",
           perDocumentLimit: 4,
           option: { sort: { sumBooking: -1 } },
           populate: {
-            path: 'imageId',
-            select: '_id imageUrl',
-            perDocumentLimit: 1
-          }
+            path: "imageId",
+            select: "_id imageUrl",
+            perDocumentLimit: 1,
+          },
         });
 
       const traveler = await Traveler.find();
@@ -48,10 +50,11 @@ module.exports = {
         imageUrl: "images/testimonial1.jpg",
         name: "Happy Family",
         rate: 4.55,
-        content: "What a great trip with my family and I should try again next time soon ...",
+        content:
+          "What a great trip with my family and I should try again next time soon ...",
         familyName: "Smitty",
         familyOccupation: "Product Designer",
-      }
+      };
 
       res.status(200).json({
         hero: {
@@ -61,12 +64,11 @@ module.exports = {
         },
         mostPicked,
         category,
-        testimonial
+        testimonial,
       });
-
     } catch (error) {
       console.log(error);
-      res.status(500).json({ message: 'Internal server error' });
+      res.status(500).json({ message: "Internal server error" });
     }
   },
 
@@ -74,9 +76,9 @@ module.exports = {
     try {
       const { id } = req.params;
       const item = await Item.findOne({ _id: id })
-        .populate({ path: 'featureId', select: '_id name qty imageUrl' })
-        .populate({ path: 'activityId', select: '_id name type imageUrl' })
-        .populate({ path: 'imageId', select: '_id imageUrl' });
+        .populate({ path: "featureId", select: "_id name qty imageUrl" })
+        .populate({ path: "activityId", select: "_id name type imageUrl" })
+        .populate({ path: "imageId", select: "_id imageUrl" });
 
       const bank = await Bank.find();
 
@@ -85,18 +87,97 @@ module.exports = {
         imageUrl: "images/testimonial2.jpg",
         name: "Happy Family",
         rate: 4.55,
-        content: "What a great trip with my family and I should try again next time soon ...",
+        content:
+          "What a great trip with my family and I should try again next time soon ...",
         familyName: "Smitty",
         familyOccupation: "Product Designer",
-      }
+      };
 
       res.status(200).json({
         ...item._doc,
         bank,
-        testimonial
-      })
+        testimonial,
+      });
     } catch (error) {
-
+      res.status(500).json({ message: "Internal server error" });
     }
-  }
+  },
+
+  bookingPage: async (req, res) => {
+    const {
+      idItem,
+      duration,
+      bookingStartDate,
+      bookingEndDate,
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      accountHolder,
+      bankFrom,
+    } = req.body;
+
+    if (!req.file) {
+      return res.status(404).json({ message: "Image not found" });
+    }
+
+    if (
+      idItem === undefined ||
+      duration === undefined ||
+      bookingStartDate === undefined ||
+      bookingEndDate === undefined ||
+      firstName === undefined ||
+      lastName === undefined ||
+      email === undefined ||
+      phoneNumber === undefined ||
+      accountHolder === undefined ||
+      bankFrom === undefined
+    ) {
+      return res.status(404).json({ message: "Lengkapi semua field" });
+    }
+
+    const item = await Item.findOne({ _id: idItem });
+    if (!item) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+
+    item.sumBooking += 1;
+
+    await item.save();
+
+    let total = item.price * duration;
+    let tax = total * 0.1;
+
+    const invoice = Math.floor(1000000 + Math.random() * 9000000);
+
+    const member = await Member.create({
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+    });
+
+    const newBooking = {
+      invoice,
+      bookingStartDate,
+      bookingEndDate,
+      total: (total += tax),
+      itemId: {
+        _id: item.id,
+        title: item.title,
+        price: item.price,
+        duration: duration,
+      },
+      memberId: member.id,
+      payments: {
+        proofPayment: `images/${req.file.filename}`,
+        bankFrom: bankFrom,
+        accountHolder: accountHolder,
+      },
+    };
+
+    const booking = await Booking.create(newBooking);
+
+    res.status(201).json({ message: "Success Booking", booking });
+  },
 };
