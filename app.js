e274262a22acde33904dea62caadb315e750aa32
Heri Setiawan <hsetiawan8.hs@gmail.com>
2022-03-26 22:57:43 +0700

    add cors pust

--- app.js
+++ app.js
@@ -39,7 +39,14 @@ app.use(
 );
 app.use(flash());
 
-app.use(cors())
+// app.use(cors());
+app.use((req,res, next)=>{
+  res.setHeader('Access-Control-Allow-Origin',"http://localhost:3000");
+  res.setHeader('Access-Control-Allow-Headers',"*");
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
